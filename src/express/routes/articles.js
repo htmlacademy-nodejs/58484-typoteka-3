168d5aba7c18b510b67942136708feb7e26a1c88
Vasya Ponomarenko <777Vasya77@gmail.com>
2020-11-29 23:14:30 +0200

    Выполнил задание #16. Личный проект: шаблонизируй это

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -5,9 +5,9 @@ const articlesRouter = new Router();
 const articleController = require(`../controllers/article-controller`);
 const articleCategoryController = require(`../controllers/article-category-controller`);
 
+articlesRouter.get(`/add`, articleController.create);
 articlesRouter.get(`/:id`, articleController.show);
 articlesRouter.get(`/edit/:id`, articleController.edit);
-articlesRouter.get(`/add`, articleController.create);
 articlesRouter.get(`/category/:id`, articleCategoryController.show);
 
 module.exports = articlesRouter;
