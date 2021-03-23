02984f334d7046f8a88a6c5c3641bf55fa2a7ad8
Vasya Ponomarenko <777Vasya77@gmail.com>
2021-03-23 20:50:05 +0200

    Исправил код по комментариям ментора

--- src/service/cli/factories/user-factory.js
+++ src/service/cli/factories/user-factory.js
@@ -1,28 +1,21 @@
 'use strict';
 
 const faker = require(`faker`);
-const {getRandomItems} = require(`../../../utils`);
+const {getRandomInt} = require(`../../../utils`);
 
 faker.locale = `ru`;
-
-const AVATARS = [
-  `avatar-1`,
-  `avatar-2`,
-  `avatar-3`,
-  `avatar-4`,
-  `avatar-5`,
-];
 
 const createUser = (userData) => {
   // faker.name.firstName - генерит латиницу;
   const [firstName, lastName] = faker.name.findName().split(` `);
+  const avatar = `avatar-${getRandomInt(1, 5)}`;
 
   return {
     email: faker.internet.email(),
     password: faker.internet.password(),
     firstName,
     lastName,
-    avatar: getRandomItems(AVATARS)[0],
+    avatar,
     ...userData,
   };
 };
