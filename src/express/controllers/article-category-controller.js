6e0f66d4df604c06e82832cfedb6187ab04fcaaf
Vasya Ponomarenko <777Vasya77@gmail.com>
2021-08-20 13:18:50 +0300

    Вынес категории в отдельный компонент

--- src/express/controllers/article-category-controller.js
+++ src/express/controllers/article-category-controller.js
@@ -5,8 +5,13 @@ const api = require(`../api`).getAPI();
 const show = async (req, res) => {
   const categoryId = req.params.id;
   const articles = await api.getArticles({categoryId, comments: true});
-  // TODO: пагинация, активная категория
-  return res.render(`articles-by-category`, {articles});
+  const categories = await api.getCategories(true);
+  // TODO: пагинация, выборка постов по категории
+  return res.render(`articles-by-category`, {
+    articles,
+    categories,
+    currentCategoryId: categoryId
+  });
 };
 
 module.exports = {
